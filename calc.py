6e560ebc0dfd3982b906c0293db5d8f2351c8e3e
emokido <gusah0p@gmail.com>
2023-04-11 21:51:25 +0900

    안내메시지 표출

--- calc.py
+++ calc.py
@@ -1,25 +1,28 @@
 # get two integer parameters
 # return sum
 def plus(x, y):
-    return x+y
+    return x + y
 
 # main function
 def main():
     check = 1
-    print("Welcome to calcuator")
-    while check >= 1:        
+    print("Welcome to calculator")
+    while check >= 1:
         print("0: exit, 1: plus")
-        check = int(input())
-        if check == 1:
-            print("First Number")
-            x = int(input())
-            print("Second Number")
-            y = int(input())
-            print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
-        else:
-            print("Thank you")
+        try:
+            check = int(input())
+            if check == 1:
+                print("First Number:")
+                x = int(input())
+                print("Second Number:")
+                y = int(input())
+                print("Answer:", plus(x, y))
+            elif check > 1:
+                print("Unsupported")
+            else:
+                print("Thank you")
+        except ValueError:
+            print("숫자를 입력하세요")
 
 if __name__ == "__main__":
     main()

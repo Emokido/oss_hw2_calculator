ed94aff83a1db113c8a0d23ff2b8dd1c28f2cd86
G <mungeon7142@gmail.com>
2023-04-12 10:48:35 +0900

    add operator and can float

--- calc.py
+++ calc.py
@@ -1,26 +1,48 @@
 # get two integer parameters
 # return sum
 def plus(x, y):
-    return x + y
+    return x+y
+def sub(x, y):
+    return x-y
+def mul(x, y):
+    return x*y
+def div(x, y):
+    return x/y
 
 # main function
 def main():
     check = 1
     print("Welcome to calculator")
     while check >= 1:
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus 2: subtract 3: multiplication 4: divide")
         try:
             check = int(input())
             if check == 1:
                 print("First Number:")
-                x = int(input())
+                x = float(input())
                 print("Second Number:")
-                y = int(input())
+                y = float(input())
                 print("Answer:", plus(x, y))
-            elif check > 1:
+            elif check == 2:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", sub(x, y))
+            elif check == 3:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", mul(x, y))
+            elif check == 4:
+                print("First Number:")
+                x = float(input())
+                print("Second Number:")
+                y = float(input())
+                print("Answer:", div(x, y))
+            elif check > 4:
                 print("Unsupported")
-            else:
-                print("Thank you")
         except ValueError:
             print("숫자를 입력하세요")
 
